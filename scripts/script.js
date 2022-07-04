773eaab398650e4fc7eb9ee999e1d3a0ebfff805
hade <darkusfr332@gmail.com>
2022-07-04 19:16:37 +0300

    add a graphical gui

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,67 @@
+const buttons  = Array.from(document.querySelectorAll('.user'));
+let computerScore =0;
+let userScore = 0;
+let state = document.querySelector(".state");
+let result = document.querySelector('.result');
 function computerPlay() {
-    let gameChoices = ['rock','paper','scissors'];
-    let randomSlect = Math.floor(Math.random()*(3));
+    let gameChoices = ['Rock','Paper','Scissors'];
+    let randomSlect = Math.floor(Math.random()*3);
     return gameChoices[randomSlect];
 }
-
-function playRound(computerSelction,playerSelection) { 
-
-    if(!playerSelection) 
-    return false;
-    
-    let playerSelect = playerSelection.toLowerCase();
-
-    if((playerSelect === 'rock' && computerSelction === 'paper')
-    ||(playerSelect === 'scissors' && computerSelction === 'rock')
-    ||(playerSelect === 'paper' && computerSelction ==='scissors')) {
-
-        return `you lose ! ${computerSelction} Beats ${playerSelect}`;
+function endGame() { 
+    if(userScore > computerScore) { 
+        state.textContent = "You Win the Game! and ... the Earth";
+        state.style.color = "green";
     }
-    else if(computerSelction === playerSelect)
-    return "Tie state";
-
-    else
-    return `you win ! ${playerSelect} Beats ${computerSelction}`; 
+    else { 
+        state.textContent = "Oops! seems to be you lost and Machines acupy the Earth";
+        state.style.color = 'red';
+    }
+    const tryAgain = document.createElement("button");
+    tryAgain.textContent = "Try Again";
+    tryAgain.setAttribute('class','tryagain');
+    state.appendChild(tryAgain);
+}
+function checkResult(computerSelection,playerSelect) {
+    if((playerSelect === 'Rock' && computerSelection === 'Paper')
+    ||(playerSelect === 'Scissors' && computerSelection === 'Rock')
+    ||(playerSelect === 'Paper' && computerSelection ==='Scissors')) {
+    computerScore++;
+    result.innerHTML = `${userScore}-${computerScore} `;
+    state.textContent = `you lose ! ${computerSelection} Beats ${playerSelect}`;
+    state.style.color = "red";
+}
+else if(computerSelection === playerSelect) { 
+    state.textContent = "Tie state";
+    state.style.color = "gray";
+}
+else { 
+    userScore++;
+    result.innerHTML = `${userScore}-${computerScore} `;
+    state.textContent = `you win ! ${playerSelect} Beats ${computerSelection}`;
+    state.style.color = 'green'; 
+}
+}
+function playRound(e) {
+    if(userScore >=5 || computerScore >=5) { 
+        endGame();
+    } 
+    else{ 
+        let playerSelect = this.classList.item(0);
+        checkResult(computerPlay(),playerSelect);
+}
 }
 
-function game() { 
-    let score = 0;
-    let i=0;
-
-    while(i<5) {
-        let userinput = prompt('Enter paper , scissors or rock to start playing');
-        let result = playRound(computerPlay(),userinput);
-
-        if(!result) 
-        console.log("invalid input");
-
-        else if(result.includes("win")) {
-        score++;
-            console.log(result);
-            i++;
-        }
-
-        else if(result.includes('lose')) {
-            i++;
-            console.log(result);
-        }
-        else 
-        console.log(result);
+buttons.forEach(button => {
+    button.addEventListener('click',playRound,{capture: true});
+});
+window.addEventListener('click',(e)=> { 
+    if(e.target.getAttribute('class') ==="tryagain") {
+        window.location.reload();
     }
-
-    let computerScore = 5 - score;
-
-    if(computerScore > score) 
-    console.log(`you lost ${computerScore} , ${score}`);
-
-    else if(computerScore === score)
-    console.log('Tie State ');
-
-    else 
-    console.log(`you win ${score} , ${computerScore}`);
+})
+function removeSelection(buttons) { 
+    buttons.forEach(button=> { 
+        button.classList.remove('selected');
+        });
 }
-game();
